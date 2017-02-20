4a4392354adefcf19eec89da47f32ac41131e324
zachakbar <zach@thinkbigfish.com>
2017-02-20 13:38:25 -0700

    removed function
    the mobile menu has been removed and will be added in a different repo so this was no longer needed for the boilerplate.

--- _source/js/_functions.js
+++ _source/js/_functions.js
@@ -1,16 +1,3 @@
 /************************************************************************/
 /* JAVASCRIPT FUNCTIONS
 /************************************************************************/
-
-function initMobileNav(){
-	
-	var menu = $('#menu'),
-		nav_btn = $('#nav_btn');
-
-	nav_btn.click(function() {
-		nav_btn.toggleClass('active');
-		nav_btn.find('#nav_icon').toggleClass('open');
-		menu.slideToggle('fast');
-		return false;
-	});
-}
