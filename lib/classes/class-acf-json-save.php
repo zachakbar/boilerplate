7ab72767710af3126e8ec3ce9b89cab280d00de0
Brett Pollett <brett@darkhousedevelopment.com>
2020-06-30 15:50:27 -0700

    fixed json field save and added back in acf nav menu fields:

--- lib/classes/class-acf-json-save.php
+++ lib/classes/class-acf-json-save.php
@@ -1,14 +1,18 @@
 <?php
 
-new tdc_acf_group_save();
+if( !defined( 'ABSPATH' ) ): exit(); endif;
+
+// list of field group IDs used in my plugin
+$acf_dir = get_stylesheet_directory() . "/acf-json";
+$acf_groups = array_diff(scandir($acf_dir), array('..', '.'));
+foreach($acf_groups as $key => $acf_group_filename):
+	$acf_groups[$key] = basename($acf_group_filename, '.json');
+endforeach;
 
 if(!class_exists('tdc_acf_group_save')):
 	class tdc_acf_group_save {
 
-	  // list of field group IDs used in my plugin
-	  private $acf_dir = get_stylesheet_directory()."/acf-json";
-
-	  private $groups = array_diff(scandir($this->acf_dir), array('..', '.'));;
+	  //private $groups = $acf_groups;
 
 	  public function __construct() {
 	    // add fitler before acf saves a group
@@ -17,7 +21,7 @@ if(!class_exists('tdc_acf_group_save')):
 
 	  public function update_field_group($group) {
 	    // called when ACF save the field group to the DB
-	    if (in_array($group['key'], $this->groups)) {
+	    if (in_array($group['key'], $acf_groups)) {
 	      // if it is one of my groups then add a filter on the save location
 	      // high priority to make sure it is not overrridded, I hope
 	      add_filter('acf/settings/save_json',  array($this, 'override_location'), 9999);
@@ -29,9 +33,12 @@ if(!class_exists('tdc_acf_group_save')):
 	    // remove this filter so it will not effect other goups
 	    remove_filter('acf/settings/save_json',  array($this, 'override_location'), 9999);
 	    // override save path
-	    $path = dirname($this->acf_dir);
+	    $path = dirname($acf_dir);
 	    return $path;
 	  } // end public function override_json_location
 
 	} // end class my_pluging_name_acf_group_save
+
+	new tdc_acf_group_save();
+
 endif;
